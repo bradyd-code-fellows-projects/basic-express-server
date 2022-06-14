21d36a711cb968265b3e439942d78266e9b89318
Brady <brady.davenport@pm.me>
2022-06-14 16:55:48 -0600

    built validator

---
+++ src/middleware/validator.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function validateQuery(req, res, next) {
+  let { name } = req.query;
+  if (!name) {
+    next('Please enter a valid query parameter, like this: /hello?name=John');
+  } else {
+    console.log('name: ', name);
+  }
+  next();
+}
+
+module.exports = { validateQuery };
