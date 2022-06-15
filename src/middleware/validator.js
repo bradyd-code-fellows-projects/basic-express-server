6c9b767de384034fbe342115d75fc9cae9e5c7fa
Brady <brady.davenport@pm.me>
2022-06-15 11:16:19 -0600

    updates response test

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -3,7 +3,7 @@
 function validateQuery(req, res, next) {
   let { name } = req.query;
   if (!name) {
-    next('Please enter a valid query parameter, like this: /hello?name=John');
+    next('Please enter a valid query parameter, like this: /person?name=John');
   } else {
     console.log('name: ', name);
   }
